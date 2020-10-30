cd899606a37acaedbbdf25bbae46025a0a83795b
briandthi <68909379+briandthi@users.noreply.github.com>
2020-10-30 09:53:30 +0100

    Initial commit

---
+++ utils2.py
@@ -0,0 +1,15 @@
+import tensorflow as tf
+
+def load_embedding():
+    return 1
+
+def csv_reader_dataset(filepaths, repeat=1, n_readers=5,
+                       n_read_threads=None, shuffle_buffer_size=10000,
+                       n_parse_threads=5, batch_size=32):
+    dataset = tf.data.Dataset.list_files(filepaths).repeat(repeat)
+    dataset = dataset.interleave(
+        lambda filepath: tf.data.TextLineDataset(filepath).skip(1),
+        cycle_length=n_readers, num_parallel_calls=n_read_threads)
+    dataset = dataset.shuffle(shuffle_buffer_size)
+    dataset = dataset.batch(batch_size)
+    return dataset.prefetch(1)
