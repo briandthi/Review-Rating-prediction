2359d03376cb804d5da2d0043a1ab0456d2c94ed
briandthi <briandthi@gmail.com>
2020-10-31 08:45:51 +0100

    prerpocess

--- utils2.py
+++ utils2.py
@@ -1,4 +1,6 @@
 import tensorflow as tf
+import numpy as np
+import re
 
 def load_embedding():
     return 1
@@ -13,3 +15,26 @@ def csv_reader_dataset(filepaths, repeat=1, n_readers=5,
     dataset = dataset.shuffle(shuffle_buffer_size)
     dataset = dataset.batch(batch_size)
     return dataset.prefetch(1)
+
+def read_glove_vecs(glove_file = 'data/glove.6B.50d.txt'):
+    with open(glove_file, 'r', encoding="utf8") as f:
+        words = set()
+        word_to_vec_map = {}
+        for line in f:
+            line = line.strip().split()
+            curr_word = line[0]
+            words.add(curr_word)
+            word_to_vec_map[curr_word] = np.array(line[1:], dtype=np.float64)
+        
+        i = 1
+        words_to_index = {}
+        index_to_words = {}
+        for w in sorted(words):
+            words_to_index[w] = i
+            index_to_words[i] = w
+            i = i + 1
+    return words_to_index, index_to_words, word_to_vec_map
+
+def ponctuation_padding(sentence):
+    s = re.sub("(?<! )(?=[.,!?()/'])|(?<=[.,!?()/'])(?! )", r' ', sentence)
+    return s
